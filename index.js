ac7acba68db7d2fddf29db52f7177e8863e2f8c7
praditya7 <mishrapraditya2001@gmail.com>
2024-08-04 07:59:49 +0530

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const axios = require("axios");
 const TelegramBot = require("node-telegram-bot-api");
 
-// Replace with your actual bot token and chat ID
-const telegramBotToken = "7443063848:AAF9Qvh1YIWl4sMRIpXrmVgTiKgozZIcjs0";
-const chatID = "-1002218125397"; // Replace with your channel or chat ID
+const telegramBotToken = "7443063848:AAF9Qvh1YIWl4sMRIpXrmVgTiKgozZIcjs0"; //BotID generated using BotFather
+const chatID = "-1002218125397"; // ChatID of the group
 
-// Initialize the Telegram bot
+// Initializing the Telegram Bot
 const bot = new TelegramBot(telegramBotToken, { polling: true });
 
-// Function to fetch a random article title
+// Function to fetch a Wikipedia article title using their API
 async function getArticle() {
   const base_url = "https://en.wikipedia.org/w/api.php";
   const params = {
@@ -29,7 +28,7 @@ async function getArticle() {
   }
 }
 
-// Function to fetch the summary of an article
+// Function to fetch the Summary of the article fetched using the same Wikipedia API
 async function getSummary(articleTitle) {
   const base_url = "https://en.wikipedia.org/w/api.php";
   const params = {
@@ -68,9 +67,12 @@ async function postArticleToTelegram(chatID) {
   }
 }
 
+//this is an Event Listener provided by the node-telegram-bot-API
+//I have used /newarticle to generate a new random Wikipedia article
 bot.onText(/\/newarticle/, (msg) => {
   const chatID = msg.chat.id;
   postArticleToTelegram(chatID);
 });
 
+//For executing the post article feature initially
 postArticleToTelegram(chatID);
